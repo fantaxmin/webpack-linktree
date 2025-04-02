e83ead9fb252c42e9566a91ef9c6eb13de39968c
Nicolás Torres L <nicolas.torreslop@gmail.com>
2025-04-01 21:11:15 -0500

    fix(webpack.config): fix config to add imgs and docs

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import { dirname } from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,7 +11,8 @@ export default {
     entry : './src/index.ts',
     output : {
         path : path.resolve(__dirname, 'dist'),
-        filename : 'main.js'
+        filename : 'main.js',
+        publicPath : './'
     },
     resolve : {
         extensions : ['.ts', '.js']
@@ -18,17 +20,28 @@ export default {
     module : {
         rules : [
             {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
+                test : /\.ts$/,
+                use : 'ts-loader',
+                exclude : /node_modules/
             }
         ]
     },
-    plugins: [
+    plugins : [
         new HtmlWebpackPlugin({
             inject: true,
             template: './public/index.html',
             filename : './index.html'
+        }),
+        new CopyWebpackPlugin({
+            patterns : [
+                {
+                    from : 'public',
+                    to : '.',
+                    globOptions: {
+                        ignore: ['**/index.html'] // Ignorar index.html al copiar
+                    }
+                }
+            ]
         })
     ]
 }
