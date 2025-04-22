e9a733045ee344df0c7ee47d3f7e91c931fd8944
Nicolás Torres L <nicolas.torreslop@gmail.com>
2025-04-22 08:39:19 -0500

    fix(download): fix curriculum download

--- src/index.ts
+++ src/index.ts
@@ -57,11 +57,17 @@ if ($buttonResume) {
 
 function downloadResume(lang : string) : void {
     const url = lang === 'ES'
-               ? '/docs/CV_NicolasTorresL_ES.pdf'
-               : '/docs/CV_NicolasTorresL_EN.pdf';
+               ? 'docs/CV_NicolasTorresL_ES.pdf'
+               : 'docs/CV_NicolasTorresL_EN.pdf';
     
     // Abrir el PDF en una nueva pestaña
-    window.open(url, '_blank');
+    const $link : HTMLAnchorElement = document.createElement('a'); // HTMLAnchorElement representan hipervinculos
+    $link.href = url;
+    $link.download = url.split('/').pop() ?? '';
+
+    document.body.appendChild($link);
+    $link.click();
+    document.body.removeChild($link);
     
     $dialog?.close();
 }
